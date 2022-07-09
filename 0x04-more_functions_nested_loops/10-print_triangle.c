dc8b6889116a9d47d2f6dd8007579be70d261842
uchario <uchario@yahoo.com>
2022-07-09 14:31:00 -0700

    added 10-main.c 10-print_triangle.c 10-print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+int _putchar(char);
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j <= (size - i))
+					_putchar(' ');
+				else
+					_putchar('#');
+			}	
+			_putchar('\n');
+		}
+	}
+}
