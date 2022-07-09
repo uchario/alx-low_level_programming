77bc3c39da2d172631eddf8603a9f3afff58113f
uchario <uchario@yahoo.com>
2022-07-09 14:29:55 -0700

    added 9-fizz_buzz.c 9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
